a64ced80ea166e9e588de883db8a5f3f826f80cb
cianpm01 <78463470+cianpm01@users.noreply.github.com>
2022-04-21 01:41:52 -0700

    server rewrite

--- public/javascripts/server.js
+++ public/javascripts/server.js
@@ -1,23 +1,17 @@
-// This is a filler comment for Github
-const server = require('server');
-const { get, socket } = server.router;
-const { render } = server.reply;
+const io = require('socket.io')(3000)
 
-// Update everyone with the current user count
-const updateCounter = ctx => {
-	ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
-};
+const users = {}
 
-
-server([
-	get('/', ctx => render('index.html')),
-  	//Socket router to call the updateCounter method when a new user joins the room
-	socket('connect', updateCounter),
-  	//socket router to call the updateCounter method when a user leaves the chatroom
-	socket('disconnect', updateCounter),
-	//socket router to send a message 
-  	socket('message', ctx => {
-    		console.log(ctx.data);
-    		ctx.io.emit('message', ctx.data);
-  	})
-]);
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
