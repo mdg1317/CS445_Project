4e699bdb93ad1133b9c05495a2ee821a2a6fe86e
cianpm01 <78463470+cianpm01@users.noreply.github.com>
2022-04-21 01:42:39 -0700

    chat rewrite

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,114 +1,34 @@
-/*var ws_url = "ws://10.17.6.221:5000";
-var websocket = new WebSocket(ws_url);
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
-websocket.onopen = function(event){
-	alert("[open] Connection established");
-	alert("Sending to server");
-	MessageAdd('<div class="message green">You have entered the chatroom.</div>');
-};
+const name = prompt('Enter your username: ')
+appendMessage('You joined')
+socket.emit('new-user', name)
 
-websocket.onclose = function(event){
-	MessageAdd('<div class="message blue">You have been disconnected.</div>');
-};
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
 
-websocket.onerror = function(event){
-	MessageAdd('<div class="message red">Connection to chat failed.</div>');
-};
+socket.on('user-connected', name => {
+  appendMessage(`${name} joined`)
+})
 
-websocket.onmessage = function(event){
-	var data = JSON.parse(event.data);
-	if(data.type == "message"){
-		MessageAdd('<div class="message">' + data.username + ': ' + data.message + '</div>');
-	}
-};
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
 
-document.getElementById("char-form").addEventListener("submit", function(event) {
-	event.preventDefault();
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const message = messageInput.value
+  appendMessage(`You: ${message}`)
+  socket.emit('send-chat-message', message)
+  messageInput.value = ''
+})
 
-	var message_element = document.getElementsByTagName("input")[0];
-	var message = message_element.value;
-
-	if(message.toString().length) {
-		var username = localStorage.getItem("username");
-		var data = {
-			type: "message",
-			username: username,
-		};
-
-		websocket.send(JSON.stringify(data));
-		message_element.value = "";
-	}
-}, false);
-
-function Username() {
-	var username = window.prompt("Enter your username:", "");
-
-	if(username.toString().length > 2){
-		localStorage.setItem("username", username);
-	} else {
-		alert("Your username must be at least two characters");
-		Username();
-	}
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
 }
-
-Username();
-
-function MessageAdd(message) {
-	var char_messages = document.getElementById("chat-messages");
-	chat_messages.insertAdjacentHTML("beforeend", message);
-	chat_messages.scrollTop = chat_messages.scrollHeight;
-}*/
-
-// Get username from cookies
-/*let user = cookie.get('user');
-if (!user) {
-	// Ask for the username if there is none set already
-	user = prompt('Enter username:');
-	if(!user){
-		alert('We cannot work with you like that!');
-	} else {
-		// Store it in the cookies for future use
-		cookie.set('user', user);
-	}
-}*/
-
-var user = prompt("Enter username:");
-if(!user){
-	alert("Invalid username");
-} else {
-	var element = document.getElementById("user1");
-	element.innerHTML = user;
-}
-
-
-var socket = io();
-
-
-socket.on('count', function (data) {
-  $('.user-count').html(data);
-});
-
-
-socket.on('message', function (data) {
-  $('.chat').append('<p><strong>' + data.name1 + '</strong>: ' + data.message + '</p>');
-});
-
-
-$('form').submit(function (e) {
-  
-  e.preventDefault();
-
-  
-  var message = $(e.target).find('input').val();
-
- 
-  socket.emit('message', {
-    user: cookie.get('user') || 'Anonymous',
-    message: message
-  });
-
- 
-  e.target.reset();
-  $(e.target).find('input').focus();
-});
-
