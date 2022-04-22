1b40b3e28edc17068eb2785bfe5a31e383e5dd77
cianpm01 <78463470+cianpm01@users.noreply.github.com>
2022-04-21 20:24:40 -0700

    annotated server.js

--- public/server.js
+++ public/server.js
@@ -1,17 +1,29 @@
+//creates server on port 3000
 const io = require('socket.io')(3000)
 
+//list of users currently connected to the chatroom
 const users = {}
-
+//calls this function when a user connects to the server
 io.on('connection', socket => {
+  //executes when the "new user" event occurs
   socket.on('new-user', name => {
+    //adds new user's name to the list of names
     users[socket.id] = name
+    //broadcasts message that a new user joined on the server
     socket.broadcast.emit('user-connected', name)
   })
+  
+  //executes during the send message event 
   socket.on('send-chat-message', message => {
+    //broadcasts message to the server
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
   })
+  
+  //executes during the disconnect event
   socket.on('disconnect', () => {
+    //broadcasts to the server that a user disconnected
     socket.broadcast.emit('user-disconnected', users[socket.id])
+    //deletes user from list of users
     delete users[socket.id]
   })
 })
