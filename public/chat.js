6732ec652196aea1ee420c4a45644017b229c66f
cianpm01 <78463470+cianpm01@users.noreply.github.com>
2022-04-22 00:48:51 -0700

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,7 @@
+//References for this part of the project
+//https://orangeable.com/javascript/chat
+//https://betterprogramming.pub/build-a-chat-app-in-node-js-with-room-functionality-6ebbd4b19133
+//
 //socket variable storing location where the server is hosting the application
 const socket = io('http://localhost:3000')
 //variable which represents a section of the html page were messages sent from other users go
@@ -9,24 +13,24 @@ const messageInput = document.getElementById('message-input')
 
 //prompts user to enter a username
 const name = prompt('Enter your username: ')
-//appends a message to user's screen saying they joined
-appendMessage('You joined')
+//displays a message to user's screen saying they joined
+displayMessage('You joined')
 //sends the name of the new user to the server, titles the event "new-user" so the server knows what to do with this information
 socket.emit('new-user', name)
 
 //this triggers when the chat-message occurs, or when a someone sends a new message
 socket.on('chat-message', data => {
-  //appends the message to the user's screen along with the name of the sender
-  appendMessage(`${data.name}: ${data.message}: ${data.date}`)
+  //displays the message to the user's screen along with the name of the sender
+  displayMessage(`${data.name}: ${data.message}: ${data.date}`)
 })
 //this triggers when the "user-connected" message occurs, or when a new user joins the chatroom
 socket.on('user-connected', name => {
   //posts to the user's screen a message saying who joined
-  appendMessage(`${name} joined`)
+  displayMessage(`${name} joined`)
 })
 
 socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
+  displayMessage(`${name} disconnected`)
 })
 
 //listener event that listens to the submit button
@@ -35,8 +39,8 @@ messageForm.addEventListener('submit', e => {
   //message meant to be sent
   const message = messageInput.value
   //this event triggers when a user tries to send a message by clicking the submit button
-  //when this happens, we call appendMessage to post some text to their screen saying that they sent a message
-  appendMessage(`You: ${message}`)
+  //when this happens, we call displayMessage to post some text to their screen saying that they sent a message
+  displayMessage(`You: ${message}`)
   //send the message to the server and
   //titles the name of this event "send-chat-message" so the server knows how to process it
   socket.emit('send-chat-message', message)
@@ -45,8 +49,8 @@ messageForm.addEventListener('submit', e => {
 })
 
 //function that posts the arguement to the html page by creating a new div on the html page and setting the text within that div to the arguement
-function appendMessage(message) {
-  //set variable equal to the string that appendMessage is being called on
+function displayMessage(message) {
+  //set variable equal to the string that displayMessage is being called on
   const messageElement = document.createElement('div')
   messageElement.innerText = message
   //append the message to a messageContainer object
